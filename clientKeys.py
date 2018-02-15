ebcd69ec0016a996951333ceaefc6c071888ec60
skyhoffert <skyhoffert@gmail.com>
2018-02-15 15:43:39 +0000

    Added dev files

---
+++ clientKeys.py
@@ -0,0 +1,4 @@
+consumer_key =  '9qV58EyyTBvVVdfq5uVaZIDgy'
+consumer_secret = '8d8ZGnVEKMGYp3ndwNE7V0Eo0plvGGo1rSUlSvLBhyN6w1J8Aq'
+access_token = '2430628696-NDiwANAFHb0XbHnpcYjOJyy9qwpJSgvOiTzTLB4'
+access_token_secret = 'KIFZXC98W5eXeUBBkGYd1lHNFBNRnhw3CXxKahTQddjP3'
