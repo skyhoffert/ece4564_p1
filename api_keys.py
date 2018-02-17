988dd26b29601d300616935394e3d218b551bff9
Blake Barnes-Cook <blake@charsoft.com>
2018-02-17 18:18:05 -0500

    added api key code just gotta get text to speech and stick it all together

---
+++ api_keys.py
@@ -0,0 +1,2 @@
+api_twitter = "PNOTAdy6VEixG1GC7uLuJRvbD"
+api_wolfram = "KE3E2Y-VAXQAWY98J"
---
+++ server.py
@@ -0,0 +1,24 @@
+import wolframalpha as wa
+import clientKeys as ck
+import sys
+
+# Function takes in plain text and returns plaintext too
+def getfromwolfram(question):
+
+	# init the client from wolfram
+	client = wa.Client(api.api_wolfram)
+
+	#print the question and receive answer
+	print(question)
+	result = client.query(question)
+
+	# grab the plaintext answer
+	try:
+		answer = next(result.results).text
+	except StopIteration:
+		answer = "No Results"
+
+	# print and return
+	print(answer)
+	return answer
+
