eae81d8b7d025295a85f12f4e758d1333968023e
skyhoffert <skyhoffert@gmail.com>
2018-02-18 22:50:38 +0000

    tweep me baby

--- client.py
+++ client.py
@@ -30,13 +30,37 @@ for i in range(1, len(sys.argv)):
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print('Connecting to host at ', host, ' on port ', port)
+# set up authentication with clientKeys file
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+# connect to the API
+api = tweepy.API(auth)
 
-s.connect((host,port))
-s.send(b'Hello, world')
+# create listener
+l = MyStreamListener()
+mystream = tweepy.Stream(auth = api.auth, listener = l)
+# tell the listener to track the indicated search string
+# add 'async=True' for non-blocking call
+mystream.filter(track=[search_str])
 
-print('Receiving data with size ', size)
+# Tweepy stream listener class
+class MyStreamListener(tweepy.StreamListener):
+    # whenever a new post is made with the search string, this function is called
+    # on_status means the filter found a status update, or tweet
+    def on_status(self, status):
+        # get the string result
+        res = status.text
+        # Cut out the substring so we have just a raw question
+        res = res.replace(search_str, '')
+        
+        print('Connecting to host at ', host, ' on port ', port)
+        s.connect((host,port))
 
-data = s.recv(size)
-s.close()
-print ('Received:', data)
+        print('Sending question to server')
+        s.send(res)
+
+        print('Receiving data with size ', size)
+
+        data = s.recv(size)
+        s.close()
+        print('Received:', data)
