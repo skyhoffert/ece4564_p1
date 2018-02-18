5e6c6feec0108a79511ec77c3cbdc578abeafb81
Blake Barnes-Cook <blake@charsoft.com>
2018-02-18 17:51:32 -0500

    added argument parsing

--- client.py
+++ client.py
@@ -5,28 +5,43 @@ A simple echo client
 
 import socket
 import sys
+import argparse
 
-# declaring variables
-host = '0.0.0.0'
-port = 0000
-size = 1024
-tag  = 'Olympics'
+#PARSE THEM ARGS MATEY!
+
+parser = argparse.ArgumentParser(description='Client program of Assignment 1.')
+parser.add_argument('-s', type=str, required=True, help='The ip address of the server', dest='host')
+parser.add_argument('-p', type=int, required=True, help='The port number of the connection', dest='port')
+parser.add_argument('-z', type=int, required=True, help='The size of the socket', dest='size')
+parser.add_argument('-t', type=str, required=True, help='Hashtag to be watched', dest='hashtag')
+arguments = parser.parse_args()
+
+print(arguments.host)
+print(arguments.port)
+print(arguments.size)
+print(arguments.hashtag)
+
+# declaring variables IGNORED
+host = arguments.host
+port = arguments.port
+size = arguments.size
+tag  = arguments.hashtag
 
 # parse arguments
-for i in range(1, len(sys.argv)):
-    print('arg ', i, ': ', sys.argv[i])
-    if '-s' in sys.argv[i]:
-        host = sys.argv[i+1]
-        i += 1
-    if '-p' in sys.argv[i]:
-        port = int(sys.argv[i+1])
-        i += 1
-    if '-z' in sys.argv[i]:
-        size = int(sys.argv[i+1])
-        i += 1
-    if '-t' in sys.argv[i]:
-        tag = sys.argv[i+1]
-        i += 1
+#for i in range(1, len(sys.argv)):
+#    print('arg ', i, ': ', sys.argv[i])
+#    if '-s' in sys.argv[i]:
+#        host = sys.argv[i+1]
+#        i += 1
+#    if '-p' in sys.argv[i]:
+#        port = int(sys.argv[i+1])
+#        i += 1
+#    if '-z' in sys.argv[i]:
+#        size = int(sys.argv[i+1])
+#        i += 1
+#    if '-t' in sys.argv[i]:
+#        tag = sys.argv[i+1]
+#        i += 1
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
