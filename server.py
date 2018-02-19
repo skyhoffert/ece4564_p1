bc7562623893c520756c82ba3ad8880d1834f825
Todd Parker <todd5913@vt.edu>
2018-02-19 00:36:10 +0000

    shit works yo!

--- server.py
+++ server.py
@@ -14,7 +14,6 @@ def getfromwolfram(question):
 	client = wa.Client(api.api_wolfram)
 
 	#print the question and receive answer
-	print(question)
 	result = client.query(question)
 
 	# grab the plaintext answer
@@ -24,7 +23,6 @@ def getfromwolfram(question):
 		answer = "No Results"
 
 	# print and return
-	print(answer)
 	return answer
 
 
@@ -33,7 +31,6 @@ def text2speech(text):
 	cmd_begin = "espeak "
 	cmd_end = " 2>/dev/null"
 
-	print(text)
 
 	call([cmd_begin + text + cmd_end], shell=True)
 
@@ -41,26 +38,27 @@ def text2speech(text):
 parser = argparse.ArgumentParser(description='Client program of Assignment 1.')
 parser.add_argument('-p', type=int, required=True, help='The port number of the connection', dest='port')
 parser.add_argument('-z', type=int, required=True, help='The size of the socket', dest='size')
-parser.add_argument('-b', type=str, required=True, help='Server backlog', dest='backlog')
+parser.add_argument('-b', type=int, required=True, help='Server backlog', dest='backlog')
 arguments = parser.parse_args()
 
 port = arguments.port
 size = arguments.size
 backlog = arguments.backlog
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("[Checkpoint] Created socket at 0.0.0.0 on port " + port)
+print("[Checkpoint] Created socket at 0.0.0.0 on port", port)
 s.bind(('', port))
+s.listen()
+print("[Checkpoint] Listening for client connections")
 while True:
-	s.listen(backlog)
-	print("[Checkpoint] Listening for client connections")
 	client, address = s.accept()
-	print("[Checkpoint] Accepted client connection from"
-		  + address[0] + " on port " + address[1])
+	print("[Checkpoint] Accepted client connection from",
+		  address[0], " on port", address[1])
 	packed_data = client.recv(size)
 	data = pickle.loads(packed_data)
-	print("[Checkpoint] Received data: " + data[1])
-	hashlib.md5().update(data[1])
-	checksum = hashlib.md5().update(data[1]).hexDigest()
+	print("[Checkpoint] Received data:", data[1])
+	hasher = hashlib.md5()
+	hasher.update(data[1])
+	checksum = hasher.hexdigest()
 	if checksum == data[2]:
 		print("[Checkpoint] Checksum is VALID")
 	else:
@@ -68,19 +66,22 @@ while True:
 		continue
 	f = Fernet(data[0])
 	question = f.decrypt(data[1]).decode('utf-8')
-	print("[Checkpoint] Decrypt: Using Key: " + data[0] + " | Plaintext: " + question)
-	print("[Checkpoint] Speaking: " + question)
+	print("[Checkpoint] Decrypt: Using Key:", data[0], " | Plaintext:", question)
+	print("[Checkpoint] Speaking:" + question)
 	parsed = question.replace("$", "\$").replace("\"", "\\\"")
-	text2speech(question)
-	print("[Checkpoint] Sending question to Wolframalpha: " + question)
+	text2speech("\"" + question + "\"")
+	print("[Checkpoint] Sending question to Wolframalpha:", question)
 	answer = getfromwolfram(question)
-	print("[Checkpoint] Received answer from Wolframalpha: " + answer)
+	print("[Checkpoint] Received answer from Wolframalpha:", answer)
 	answer = f.encrypt(answer.encode('utf-8'))
-	print("[Checkpoint] Encrypt: Generated Key: " + data[0]
-		  + " | Ciphertext: " + answer)
-	checksum = hashlib.md5().update(answer).hexDigest()
+	print("[Checkpoint] Encrypt: Generated Key:", data[0],
+		  " | Ciphertext: ", answer)
+	hasher = hashlib.md5()
+	hasher.update(answer)
+	checksum = hasher.hexdigest()
+	print("[Checkpoint] Generated MD5 Checksum:", checksum)
 	payload = (answer, checksum)
 	payload = pickle.dumps(payload)
-	print("[Checkpoint] Sending data: " + payload)
+	print("[Checkpoint] Sending data:", payload)
 	client.send(payload)
 	client.close()
